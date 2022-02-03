5adf3f690207bb44c3613326cec36712d4af078d
p4nuruj <panuruj.innu@bumail.net>
2022-02-04 01:59:55 +0700

    ex04

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import './App.css';
 
 function Userlist () {
   return (
-    <div className="userlist">
+    <div className="userlist" align="center">
+    <h1 style={{color:"white"}}>UserName</h1>
     </div>
   )
 }
 
 function Chat () {
   return (
-    <div className="chat">
+    <div className="chat" >
+      <br></br><p>สวัสดีเราชื่อรุจนะ</p>
+      <p align="right">สวัสดีเราชื่อ_____</p>
+      <p>ยินดีที่ได้รู้จักนะ</p>
+      <p align="right">ยินดีที่ได้รู้จักเหมือนกัน</p>
     </div>
   )
 }
 
 function Inputbox () {
   return (
-    <div className="inputbox">
+    <div className="inputbox" align="center"><br></br>
+      <input type="text" style={{height: "30px"}} font size= "40" padding="300"></input>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="button" value="Send"></input>
     </div>
   )
 }
