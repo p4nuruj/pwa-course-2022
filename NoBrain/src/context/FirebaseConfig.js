696775e1354f7445bbe29f25bbceda56f1925ce0
p4nuruj <panuruj.innu@bumail.net>
2022-04-23 19:54:59 +0700

    a

--- NoBrain/src/context/FirebaseConfig.js
+++ NoBrain/src/context/FirebaseConfig.js
@@ -4,6 +4,7 @@ import 'firebase/compat/database';
 const firebaseConfig = {
   apiKey: "AIzaSyBac7WRagRRzYGUgDUHwIwKpiFjDbU7U4s",
   authDomain: "nobrain-e653d.firebaseapp.com",
+  databaseURL: "https://nobrain-e653d-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "nobrain-e653d",
   storageBucket: "nobrain-e653d.appspot.com",
   messagingSenderId: "96351281480",
